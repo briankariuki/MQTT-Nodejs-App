1d7c0ee844f86b5711961a6f8bb3168d69ac1d63
briankariuki <bryankariuki79@gmail.com>
2020-05-20 15:20:31 +0300

    first commit

--- server/src/app.js
+++ server/src/app.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "/public"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/public/index.html"));
+    res.sendFile(__dirname + "/public/index.html");
   });
 }
 
---
+++ server/src/public/precache-manifest.e2ce9c8dfe3de671e634732c842ca8b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c9ea79d7c849a7f6e72f7edf3de304",
+    "url": "/index.html"
+  },
+  {
+    "revision": "734117b165c7c41c0c7e",
+    "url": "/static/css/main.33e0fb70.chunk.css"
+  },
+  {
+    "revision": "e0071ca75e60a7bbbb6c",
+    "url": "/static/js/2.29aa5a9b.chunk.js"
+  },
+  {
+    "revision": "f1a03c1f2e879f3cba3e2b57eabb23eb",
+    "url": "/static/js/2.29aa5a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "734117b165c7c41c0c7e",
+    "url": "/static/js/main.3b1e0171.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "a1e460ff48272053825bf4b5735bb26b",
+    "url": "/static/media/character.a1e460ff.svg"
+  },
+  {
+    "revision": "7430a7e0bc6bb6893db7a58f7773f9db",
+    "url": "/static/media/illustration_02.7430a7e0.svg"
+  }
+]);
